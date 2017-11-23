f99231a2aebf8fb872e2895fac9a3d5155dcbcb6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-11-23 21:23:21 +0000

    Add grid highlighting and uncovering

--- main.lua
+++ main.lua
@@ -7,11 +7,12 @@ function love.load()
 end
 
 function love.update(dt)
-
+    getMousePos()
 end
 
 function love.draw()
     drawTIles(gridSizeX, gridSizeY)
+    drawMousePos()
 end
 
 function loadImages()
@@ -26,7 +27,37 @@ end
 function drawTIles(gridX, gridY)
     for x = 1, gridX do
         for y = 1, gridY do
-            love.graphics.draw(images.covered, (x - 1) * cellSize, (y - 1) * cellSize)
+            local image
+
+            if x == selectedX and y == selectedY then
+                if love.mouse.isDown(1) then
+                    image = images.uncovered
+                else
+                    image = images.coveredHighlighted
+                end
+            else
+                image = images.covered
+            end
+            love.graphics.draw(image, (x - 1) * cellSize, (y - 1) * cellSize)
         end
     end
+end
+
+function getMousePos()
+    selectedX = math.floor(love.mouse.getX() / cellSize) + 1
+    selectedY = math.floor(love.mouse.getY() / cellSize) + 1
+
+    if selectedX > gridSizeX then
+        selectedX = gridSizeX
+    end
+
+    if selectedY > gridSizeY then
+        selectedY = gridSizeY
+    end
+end
+
+function drawMousePos()
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.print("Selected x: " .. selectedX .. ", Selected y: " .. selectedY)
+    love.graphics.setColor(255, 255, 255)
 end
